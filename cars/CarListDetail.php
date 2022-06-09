bf394e7625a25c7d7afed82b40fe0d5223cfac8f
Jeppy <joel.erni@hispeed.ch>
2022-06-09 17:29:47 +0200

    php fix

--- cars/CarListDetail.php
+++ cars/CarListDetail.php
@@ -14,11 +14,6 @@
 	}
 
 	$result = mysqli_query($conn, "SELECT * FROM car WHERE rank=$rank AND fk_racetypeid = $type");
-	$result5 = mysqli_query($conn, "SELECT * FROM car WHERE rank=5 AND fk_racetypeid = $type");
-	$result4 = mysqli_query($conn, "SELECT * FROM car WHERE rank=4 AND fk_racetypeid = $type");
-	$result3 = mysqli_query($conn, "SELECT * FROM car WHERE rank=3 AND fk_racetypeid = $type");
-	$result2 = mysqli_query($conn, "SELECT * FROM car WHERE rank=2 AND fk_racetypeid = $type");
-	$result1 = mysqli_query($conn, "SELECT * FROM car WHERE rank=1 AND fk_racetypeid = $type");
 	$resultb = mysqli_query($conn, "SELECT * FROM racetype WHERE racetypeid = $type");
 	
 	while($rowb = mysqli_fetch_assoc($resultb))
